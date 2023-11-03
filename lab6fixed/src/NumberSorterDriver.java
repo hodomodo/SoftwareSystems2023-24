6c013f43d78650759e6a203a28d8efbb2707fc69
hodomodo <94387167+hodomodo@users.noreply.github.com>
2023-11-03 14:43:52 +0000

    Update NumberSorterDriver.java

--- lab6fixed/src/NumberSorterDriver.java
+++ lab6fixed/src/NumberSorterDriver.java
@@ -1,11 +1,18 @@
 
 public class NumberSorterDriver
 {
-	public static void testNumberSorter()
+	public static void main(String[] args)
 	{
 		NumberSorter sorter = new NumberSorter();
-		sorter.sort(new int [] {1,9,2,3,10,8,12,1, 99, 2, 43, 68, 109, 0});
-		sorter.sort(new int [] {1,2,3,4});
-		sorter.sort(new int [] {4,3,2,1});
+		
+		// Descending
+		sorter.sort(new int [] {1,9,2,3,10,8,12,1, 99, 2, 43, 68, 109, 0}, false);
+		sorter.sort(new int [] {1,2,3,4}, false);
+		sorter.sort(new int [] {4,3,2,1}, false);
+		
+		//Ascending
+		sorter.sort(new int [] {1,9,2,3,10,8,12,1, 99, 2, 43, 68, 109, 0}, true);
+		sorter.sort(new int [] {1,2,3,4}, true);
+		sorter.sort(new int [] {4,3,2,1}, true);
 	}
 }
